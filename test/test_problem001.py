40d21590f6ae8b29d6bb8742e26a5cc4a26fae6a
Stu W <stubillwhite@gmail.com>
2019-09-23 12:29:00 +0100

    Add some comments

--- test/test_problem001.py
+++ test/test_problem001.py
@@ -1,8 +1,9 @@
 from problem001 import *
 import unittest
 
-# Pull in the built-in Python testing framewkr
+# Pull in the built-in Python testing framework
 class Problem1TestCase(unittest.TestCase):
 
+    # Tests need to begin with test_ to be found
     def test_sum_of_divisible_naturals(self):
         self.assertEqual(sum_of_divisible_naturals(10), 23)
