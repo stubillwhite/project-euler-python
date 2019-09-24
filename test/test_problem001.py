ca66ce499d1b2bc3b22f161c5b9c635ca7a02f1e
Stu W <stubillwhite@gmail.com>
2019-09-24 13:30:47 +0100

    Restructure project

--- test/test_problem001.py
+++ test/test_problem001.py
@@ -1,4 +1,4 @@
-from problem001 import *
+from euler.problem001 import *
 import unittest
 
 # Pull in the built-in Python testing framework
@@ -7,3 +7,6 @@ class Problem1TestCase(unittest.TestCase):
     # Tests need to begin with test_ to be found
     def test_sum_of_divisible_naturals(self):
         self.assertEqual(sum_of_divisible_naturals(10), 23)
+
+    def test_deliberately_failing_test(self):
+        self.assertEqual(1 + 1, 3)
